43aa24659e608425cebf046ce9adbf5900f68cbb
Essigautomat <mw1996oede@googlemail.com>
2018-06-27 22:55:58 +0200

    removes Comments now, may breakgit add src/main/java/essigautomat/cgconvert/LineMakroInclude.java!

--- src/main/java/essigautomat/cgconvert/LineMakroInclude.java
+++ src/main/java/essigautomat/cgconvert/LineMakroInclude.java
@@ -4,13 +4,13 @@ public class LineMakroInclude extends LineBehaviour{
 	@Override
 	public boolean isPossible(String line, String convline, InfoSubscriber info) {
 		String args[] = convline.split(" ");
-		return args.length == 2 && args[0].equals("#include");
+		return args.length >= 2 && args[0].equals("#include");
 	}
 
 	@Override
 	public boolean execute(String line, String convline, InfoSubscriber info) {
 		String args[] = convline.split(" ");
-		SaveLine.get().addInclude(args[1]);
+		SaveLine.get().addInclude(StringOperations.removeComment(args[1]));
 		return true;
 	}
 }
