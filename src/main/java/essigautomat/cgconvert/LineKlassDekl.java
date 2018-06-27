58951f30acda6c8fae55c6f33f5b45f79e680db4
Essigautomat <mw1996oede@googlemail.com>
2018-06-27 18:27:27 +0200

    now doesn't add class DEKLARATIONS to SaveLine, only DEFINITIONS

--- src/main/java/essigautomat/cgconvert/LineKlassDekl.java
+++ src/main/java/essigautomat/cgconvert/LineKlassDekl.java
@@ -1,19 +1,18 @@
 package essigautomat.cgconvert;
 
-import org.apache.commons.lang.StringUtils;
-
 public class LineKlassDekl extends LineBehaviour {
 
 	@Override
 	public boolean isPossible(String line, String convline, InfoSubscriber info) {
-		return info.isHeader() && StringOperations.isKlassDef(convline);
+		return info.isHeader() && StringOperations.isKlassDefOrDek(convline);
 	}
 
 	@Override
 	public boolean execute(String line, String convline, InfoSubscriber info) {
 		String lst[] = convline.split(" ");
 		SaveLine.get().addClassDekl(StringOperations.removeSyntaxChars(lst[1]));
-		SaveLine.get().saveLevel(line, SaveLine.KLASSDEF);
+		if(!StringOperations.isKlassDekl(convline))//Its a Definition, not a Deklaration
+			SaveLine.get().saveLevel(line, SaveLine.KLASSDEF);
 		return true;
 	}
 
